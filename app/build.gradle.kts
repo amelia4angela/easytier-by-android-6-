7219596806bd7bf7e5a9c7ae034ed2ad73df4f04
amelia4angela <amelia4angela@users.noreply.github.com>
2026-06-24 10:31:09 +0800

    Bump to v0.1.2
    - P2P 节点新增绿色 P2P 标签
    - version 0.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.easytier.easytiervpn"
         minSdk = 23
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.1.2"
     }
 
     signingConfigs {
